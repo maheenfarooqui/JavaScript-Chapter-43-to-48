9327e278657b4c6ddea983539bac8e9514da0c47
maheen <maheenzuhra@gmail.com>
2026-02-22 21:51:48 +0500

    update

---
+++ app.js
@@ -0,0 +1,14 @@
+function aBox(){
+    Swal.fire({
+  title: "Good job!",
+  text: "You clicked the button!",
+  icon: "success"
+});
+};
+function clickMe(a,b){
+     Swal.fire({
+  title: a,
+  text: b,
+  icon: "success"
+});
+}
