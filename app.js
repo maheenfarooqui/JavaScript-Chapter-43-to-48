49a5a4dbd9110d121fb2f5f098dcd0b0f4fa463a
maheen <maheenzuhra@gmail.com>
2026-02-22 22:02:45 +0500

    update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// 1. Show an alert box on click on a link.
 function aBox(){
     Swal.fire({
   title: "Good job!",
@@ -5,10 +6,20 @@ function aBox(){
   icon: "success"
 });
 };
+// 2. Display some Mobile images in browser. On click on an image Show the message in alert to user.
 function clickMe(a,b){
      Swal.fire({
   title: a,
   text: b,
   icon: "success"
 });
+}
+
+// 4. Display an image in browser. Change the picture on mouseover and set the first picture on mouseout.
+function bulbOn(e){
+    e.src = "https://www.w3schools.com/js/pic_bulbon.gif";
+}
+function bulbOff(e){
+    e.src ="https://www.w3schools.com/js/pic_bulboff.gif";
+
 }
